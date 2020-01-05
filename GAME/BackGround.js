39891562d8f4b77aaa495cc958237c4825dd7adb
Mina Nagy <minanagy1993@gmail.com>
2020-01-05 20:47:55 +0200

    Add Jump and Move v1

--- GAME/BackGround.js
+++ GAME/BackGround.js
@@ -1,14 +1,24 @@
 var initX = 0;
 var initY = 0;
-
 $(document).keydown(function(e){
-    if(e.keyCode == 37) {//left
-      initX += 10;
-      document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px";
-      
-    }
-    else if(e.keyCode == 39) {//right
-      initX -= 10;
-      document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px";
-    }
+  //console.log(e.keyCode);
+  switch(e.keyCode){
+    case 37:
+        initX += 10;
+        document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px"; 
+    break;
+    case 39:
+        initX -= 10;
+        document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px";
+    break;
+    case 32:
+        $('img').animate({top:["-=100", "swing"],},100);
+        //$('img').animate({left:["+=45", "swing"],},100);
+        $('img').animate({top:["+=100", "swing"],},100);
+        /*
+            initX -= 10;
+            document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px";
+        */
+    break;
+  }
 });
