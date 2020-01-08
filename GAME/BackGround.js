2f143ce6ce3994f78a183ee769b2aeb7fc5b552a
Lamis El-Zobeidy <lamis.zobeidy@gmail.com>
2020-01-08 16:28:05 +0200

    lamis added health

--- GAME/BackGround.js
+++ GAME/BackGround.js
@@ -1,5 +1,8 @@
 var initX = 0;
 var initY = 0;
+var Counter = 5;
+var Avatar = $("body");
+$("p").html(`${Counter}`)
 $(document).keydown(function(e){
   //console.log(e.keyCode);
   switch(e.keyCode){
@@ -12,9 +15,9 @@ $(document).keydown(function(e){
         document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px";
     break;
     case 32:
-        $('img').animate({top:["-=100", "swing"],},100);
-        //$('img').animate({left:["+=45", "swing"],},100);
-        $('img').animate({top:["+=100", "swing"],},100);
+        $('.mario').animate({top:["-=100", "swing"],},100);
+        $('.mario').animate({left:["+=45", "swing"],},100);
+        $('.mario').animate({top:["+=100", "swing"],},100);
         /*
             initX -= 10;
             document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px";
@@ -22,3 +25,11 @@ $(document).keydown(function(e){
     break;
   }
 });
+Avatar.on("click",function(){
+  if(Counter!=0){
+        Counter= Counter -1 ; 
+        $("p").html(Counter);
+        console.log(Counter);
+  }
+}
+);
