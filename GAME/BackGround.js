80562e745a5004a26ef661befff981c86063fb03
Nour Fayed <nourfayedd@gmail.com>
2020-01-09 12:52:53 +0200

    Nour added incoming enemies

--- GAME/BackGround.js
+++ GAME/BackGround.js
@@ -1,8 +1,8 @@
 var initX = 0;
 var initY = 0;
-var Counter = 5;
+var health = 5;
 var Avatar = $("body");
-$("p").html(`${Counter}`)
+$("p").html(`${health}`)
 $(document).keydown(function(e){
   //console.log(e.keyCode);
   switch(e.keyCode){
@@ -26,10 +26,40 @@ $(document).keydown(function(e){
   }
 });
 Avatar.on("click",function(){
-  if(Counter!=0){
-        Counter= Counter -1 ; 
-        $("p").html(Counter);
-        console.log(Counter);
+  if(health!=0){
+        health--; 
+        $("p").html(health);
   }
 }
 );
+var enemyPositionX, enemyPositionY;
+
+function enemyMovement(){
+  
+    enemyPositionX --;
+    $(".enemy-img").css({top:enemyPositionY  , left: enemyPositionX, position:'relative'})
+    
+    
+    var rect2=document.getElementsByClassName("mario")[0].getBoundingClientRect() ;
+ 
+    var rect1= document.getElementsByClassName("enemy-img")[0].getBoundingClientRect();
+    console.log(rect1.x ,"  ,  " ,rect2.x);
+    if(rect1.x - rect1.width == rect2.x){
+      health--;
+      $("p").html(health);
+      clearInterval(MovingEnemyInterval);
+    }
+  
+}
+
+function enemyIncoming(playerLocationX,playerLocationY){
+  //console.log("INCOMIIIIIIIIIIIIIIIIIIIIIIIING!!!!! ")
+  $(".enemy-img").attr("src","enemy.gif")
+  $(".enemy-img").width(50).height(50);
+  enemyPositionX = 1200 + playerLocationX , enemyPositionY= 400 + playerLocationY ;
+  $(".enemy-img").css({ top: enemyPositionY  , left: enemyPositionX, position:'relative'})
+}
+
+
+enemyIncoming(initX,initY);
+var MovingEnemyInterval=setInterval(enemyMovement,10);
