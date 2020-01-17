d9b6d3c6d2d70cc95a0d8f475d5a6367aaf84048
fadyjan <fadyjan95@gmail.com>
2020-01-17 19:39:04 +0200

    Adding fire to Our hero first try

--- GAME/BackGround.js
+++ GAME/BackGround.js
@@ -1,65 +1,14 @@
 var initX = 0;
 var initY = 0;
-var health = 5;
-var Avatar = $("body");
-$("p").html(`${health}`)
+
 $(document).keydown(function(e){
-  //console.log(e.keyCode);
-  switch(e.keyCode){
-    case 37:
-        initX += 10;
-        document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px"; 
-    break;
-    case 39:
-        initX -= 10;
-        document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px";
-    break;
-    case 32:
-        $('.mario').animate({top:["-=100", "swing"],},100);
-        $('.mario').animate({left:["+=45", "swing"],},100);
-        $('.mario').animate({top:["+=100", "swing"],},100);
-        /*
-            initX -= 10;
-            document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px";
-        */
-    break;
-  }
-});
-Avatar.on("click",function(){
-  if(health!=0){
-        health--; 
-        $("p").html(health);
-  }
-}
-);
-var enemyPositionX, enemyPositionY;
-
-function enemyMovement(){
-  
-    enemyPositionX --;
-    $(".enemy-img").css({top:enemyPositionY  , left: enemyPositionX, position:'relative'})
-    
-    
-    var rect2=document.getElementsByClassName("mario")[0].getBoundingClientRect() ;
- 
-    var rect1= document.getElementsByClassName("enemy-img")[0].getBoundingClientRect();
-    console.log(rect1.x ,"  ,  " ,rect2.x);
-    if(rect1.x - rect1.width == rect2.x){
-      health--;
-      $("p").html(health);
-      clearInterval(MovingEnemyInterval);
+    if(e.keyCode == 37) {//left
+      initX += 10;
+      document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px";
+      
     }
-  
-}
-
-function enemyIncoming(playerLocationX,playerLocationY){
-  //console.log("INCOMIIIIIIIIIIIIIIIIIIIIIIIING!!!!! ")
-  $(".enemy-img").attr("src","enemy.gif")
-  $(".enemy-img").width(50).height(50);
-  enemyPositionX = 1200 + playerLocationX , enemyPositionY= 400 + playerLocationY ;
-  $(".enemy-img").css({ top: enemyPositionY  , left: enemyPositionX, position:'relative'})
-}
-
-
-enemyIncoming(initX,initY);
-var MovingEnemyInterval=setInterval(enemyMovement,10);
+    else if(e.keyCode == 39) {//right
+      initX -= 10;
+      document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px";
+    }
+});
--- GAME/CanvasV1/test.js
+++ GAME/CanvasV1/test.js
@@ -1,8 +1,16 @@
-var context, controller, character, loop ,img ,ob;
+var context, controller, character, loop ,img ,fire ;
+
 var initX = 0;
 var initY = 0;
-var obx =500;
-var oby =600 ;
+
+var FX=90;
+var FY=50;
+
+//var obx =500;
+//var oby =600 ;
+
+
+
 
 context = document.querySelector("canvas").getContext("2d");
 
@@ -13,8 +21,11 @@ context.canvas.width = 1800;
 img = document.getElementById("beka");
 img.style.visibility = "hidden";
 
-ob=document.getElementById("OB");
-ob.style.visibility="hidden"
+//ob=document.getElementById("OB");
+//ob.style.visibility="hidden"
+
+fire=document.getElementById("fire");
+fire.style.visibility="hidden"
 
 character = {
 
@@ -27,13 +38,19 @@ character = {
   y:0,
   y_velocity:0
 
+
+
 };
+
+
 
 controller = {
 
   left:false,
   right:false,
   up:false,
+  nar :false,
+
   keyListener:function(event) {
 
     var key_state = (event.type == "keydown")?true:false;
@@ -52,6 +69,10 @@ controller = {
  
       break;
 
+      case 13:
+        controller.nar=key_state;
+      break;
+
     }
 
   }
@@ -60,35 +81,61 @@ controller = {
 
 loop = function() {
   context.clearRect(character.x, character.y ,character.width,character.height);
-  context.clearRect(obx,oby,150,30);
+  
+  
+
+
+ // context.clearRect(obx+300,oby+20,150,30);
+ // context.clearRect(obx+700,oby+10,150,30);
+  
+
   if (controller.up && character.jumping == false) {
     
     character.y_velocity -= 50;
     character.jumping = true;
+    
 
   }
 
   if (controller.left) {
-       // initX += 10;
-        //document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px";
-        character.x_velocity -= 0.5;
+    
+
+       character.x_velocity -= 0.5;
 
   }
 
   if (controller.right) {
-
+   
     character.x_velocity += 0.5;
     initX -= 10;
-    obx -=10;
+    //obx -=10;
     document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px";  
 
   }
 
+  if(controller.nar)
+  {
+   
+    for(FX=90 ; character.x+FX <1500 ; FX=FX+0.5)
+    {
+     // FX=FX+0.1
+      
+      context.drawImage(fire ,character.x+FX, character.y+FY,30,30)
+      //context.beginPath();
+     // context.clearRect(character.x +FX, character.y+FY,30,30)
+     
+    }
+
+
+
+
+  }
+
+
+ 
   character.y_velocity +=0.5 ;// gravity
   character.x += character.x_velocity; // noooooooooooooooooooooooooooooooooooooour
-  
   character.y += character.y_velocity; // nouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuur
-
   character.x_velocity *= 0.9;// friction
   character.y_velocity *= 0.9;// friction
 
@@ -112,15 +159,15 @@ loop = function() {
 
   }
   
- 
-  //context.fillRect(0, 0, 1800, 1600);// x, y, width, height
-  
-  
-  
   context.drawImage(img, character.x, character.y ,character.width,character.height);
   context.beginPath();
-  context.drawImage(ob,obx,oby,150,30);
-  context.beginPath();
+  
+
+  //context.drawImage(ob,obx,oby,150,30);
+  //context.drawImage(ob,obx+300,oby+20,150,30);
+  //context.drawImage(ob,obx+700,oby+10,150,30);
+  //context.beginPath();
+  
   // call update when the browser is ready to draw again
   window.requestAnimationFrame(loop);
 
