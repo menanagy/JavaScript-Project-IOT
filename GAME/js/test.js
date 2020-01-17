19306c2f5a5c98cc57c059ab7d38c3ddfe25d8dd
lamis <lamis.zobeidy@gmail.com>
2020-01-17 23:11:17 +0200

    fixed image visibilty issue and flamme issue

--- GAME/js/test.js
+++ GAME/js/test.js
@@ -1,148 +1,123 @@
-var context, controller, character, loop ,img , ob ;
+var controller, character, loop;
 var initX = 0;
 var initY = 0;
-var obx =500;
-var oby =700 ;
-var obstacleWidth = 150;
-var obstacleHeight = 75;
-var newpositiony;
+var FX=90;
+var FY=30;
 
+//canvas' width and height
+const GameWidth = 1800;
+const GameHeight = 950;
+let canvas = document.getElementById("gamecanvas");
+let context = canvas.getContext('2d');
 
+//the player used in the game
+var player = document.getElementById("beka");
 
-
-context = document.querySelector("canvas").getContext("2d");
-
-
+//fire flame used to attack
+var fire = document.getElementById("fire");
+//var obx =500;
+//var oby =600 ;
+//context = document.querySelector("canvas").getContext("2d");
 context.canvas.height = 950; // 7dod el shasha el soda el marsoma fo2 el asl
 context.canvas.width = 1800;
-
-img = document.getElementById("beka");
-img.style.visibility = "hidden";
-var health = document.getElementById("health");
-health.style.visibility= "hidden";
-
-ob=document.getElementById("OB");
-ob.style.visibility="hidden"
+//var ob=document.getElementById("OB");
 
 character = {
-
-  
   height:100,
   jumping:true,
   width:100,
   x:0, // 3ayzo yenzl fen 
-  x_velocity:0,
+  x_velocity:3,
   y:0,
-  y_velocity:0
-
+  y_velocity:10
 };
 
 controller = {
-
   left:false,
   right:false,
   up:false,
+  nar :false,
   keyListener:function(event) {
-
     var key_state = (event.type == "keydown")?true:false;
-
     switch(event.keyCode) {
-
       case 37:// left key
-        controller.left = key_state;
-        
+        controller.left = key_state;  
       break;
       case 32:// up key
         controller.up = key_state;
       break;
       case 39:// right key
         controller.right = key_state;
- 
       break;
-
+      case 13:// enter key
+        controller.nar=key_state;
+      break;
     }
-
   }
-
 };
-function checkObstacleCollision(){
-  var obLen = 150;
-  for(var x=0;x<obLen;x++){
-    var obX = obx;
-    var obY = oby;
-    //console.log("hellooo");
-    console.log(character.x);
-    if((character.x + 60 > obX && character.x + 50 < obX + obstacleWidth || character.x > obX && character.x < obX + obstacleWidth) && character.y + 27 > obY - 30 && character.y + 27 < obY){
-      console.log("pika pika!!");
-     newpositiony = oby - obstacleHeight;
-     return true;
-    }/*else if((character.x + character.width > obX && character.x + character.width < obX + obstacleWidth || character.x > obX && character.x < obX + obstacleWidth) && character.y > obY + obstacleHeight && character.y < obY + obstacleHeight + 10){
-      dyn = 0;
-    }else if(character.x + character.width > obX - 10 && character.x + character.width < obX && ((character.y + character.height > obY && character.y + character.height < obY + obstacleHeight) || (character.y > obY && character.y < obY + obstacleHeight))){
-      dxp = 0;
-    }else if(character.x  > obX + obstacleWidth && character.x < obX + obstacleWidth + 10 && ((character.y + character.height > obY && character.y + character.height < obY + obstacleHeight) || (character.y > obY && character.y < obY +  obstacleHeight))){
-      dxn = 0;
-    }*/
-
-  }
-  //console.log("mana mab2olsh pika pika aho");
-  return false;
-}
 
 loop = function() {
-  context.clearRect(character.x, character.y ,character.width,character.height);
-  var checking = checkObstacleCollision();
-  context.clearRect(obx,oby,150,30);
-  
+  //context.clearRect(character.x, character.y ,character.width,character.height);
+  context.clearRect(0,0,1800,950)
+ // context.clearRect(obx+300,oby+20,150,30);
+ // context.clearRect(obx+700,oby+10,150,30);
 
   if (controller.up && character.jumping == false) {
     
     character.y_velocity -= 50;
     character.jumping = true;
-
   }
 
   if (controller.left) {
-    
-        initX += 10;
-        obx +=10;
-        document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px";
-        character.x_velocity -= 0.5;
-
+       character.x_velocity -= 0.5;
   }
 
   if (controller.right) {
-   
     character.x_velocity += 0.5;
     initX -= 10;
-    obx -=10;
+    //obx -=10;
+    console.log(initX);
     document.getElementsByTagName('body')[0].style.backgroundPositionX = initX + "px";  
+  }
+
+  if(controller.nar)
+  {
+
+    context.drawImage(fire ,character.x+FX, character.y+FY,40,40)
+    FX+=31;
+    console.log(FX)
+    if(FX>900)
+    {
+      //for(FX=90 ; character.x+FX <1800 ; FX=FX+31)
+      //{
+       // context.drawImage(fire ,character.x+FX, character.y+FY,30,30)
+        //context.beginPath();
+        //context.clearRect(character.x +FX, character.y+FY,30,30)
+       
+      //}
+      //context.clearRect(0,0,1800,950)
+      FX=90
+    }
+
+
+
 
   }
-  if(checking == true)
-  {
-    console.log("ana batzene2 hena wenty wala hena");
-    console.log(newpositiony);
-    character.y = newpositiony;
-    character.y_velocity =0;
-  }
-  else{
-    character.y += character.y_velocity;}
+
+
+ 
   character.y_velocity +=0.5 ;// gravity
-  console.log("ana henaa w banot");
   character.x += character.x_velocity; // noooooooooooooooooooooooooooooooooooooour
-  //console.log(character.x)
-   // nouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuur
-  //console.log(character.y)
+  character.y += character.y_velocity; // nouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuur
   character.x_velocity *= 0.9;// friction
   character.y_velocity *= 0.9;// friction
-  
+
   // if character is falling below floor line
   if (character.y > 800) {
 
     character.jumping = false;
     character.y = 800;
-    character.y_velocity = 10;
+    character.y_velocity = 0;
 
   }
 
@@ -157,16 +132,14 @@ loop = function() {
 
   }
   
- 
-  //context.fillRect(0, 0, 1800, 1600);// x, y, width, height
-  
-  
-  
-  context.drawImage(img, character.x, character.y ,character.width,character.height);
-  context.drawImage(health, 90, 0 ,30,30);
+  context.drawImage(player, character.x, character.y ,character.width,character.height);
   context.beginPath();
-  context.drawImage(ob,obx,oby,150,30);
-  context.beginPath();
+  
+
+  //context.drawImage(ob,obx,oby,150,30);
+  //context.drawImage(ob,obx+300,oby+20,150,30);
+  //context.drawImage(ob,obx+700,oby+10,150,30);
+  //context.beginPath();
   
   // call update when the browser is ready to draw again
   window.requestAnimationFrame(loop);
